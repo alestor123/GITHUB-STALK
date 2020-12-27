99b89e6c14779e48aaf4c73846ddf98a0f1b9dfe
alestor123 <alestoraldous@gmail.com>
2020-12-27 18:11:24 +0530

    added event act

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ var figlet = require('figlet'),
 axios = require('axios'),
 username = process.argv[2] || 'github',
 api = 'https://api.github.com/',
+acts = ["WatchEvent","PushEvent","CreateEvent","PullRequestEvent","DeleteEvent","ForkEvent","IssueCommentEvent","PullRequestReviewCommentEvent","IssuesEvent","PublicEvent",],
 chalk = require('chalk');
 figlet('Github Stalk', (err,data) => {
 if(err) throw err
@@ -12,14 +13,15 @@ axios.get(api+'users/'+username).then((response) => {
     console.log(chalk.red('👤 : ' + username + ' AKA ' + response.data.name))
 Info(response)
 Lang()
+Activity()
 })
 })
 // functions logging info 
 function Info(response){
     console.log(chalk.green('Bio : '+ response.data.bio ))
     console.log(chalk.green('Location : '+ response.data.location ))
-    console.log(chalk.green('Work : '+ response.data.company ))
     console.log(chalk.green('Web Page : '+ response.data.blog ))
+    console.log(chalk.green('Work : '+ response.data.company ))
     console.log(chalk.green('Twitter : '+ response.data.twitter_username ))
     console.log(chalk.green('Repos : '+ response.data.public_repos ))
     console.log(chalk.green('Gists : '+ response.data.public_gists ))
@@ -34,4 +36,9 @@ axios.get(api+'users/'+username+'/repos').then((response) => {
               console.log('🌈 ' + chalk.bold.yellow('Langs'));
         console.log(chalk.green(languages.join(" ")));
 })
+}
+function Activity() {
+    axios.get(api+'users/'+username+'/events').then((response) => {
+console.log(response.data)
+    })  
 }
