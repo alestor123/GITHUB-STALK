d24b2d115be1bd87ae9adc8d1158b6d7957fc734
alestor123 <alestoraldous@gmail.com>
2020-12-27 13:34:43 +0530

    added nameee

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 var figlet = require('figlet'),
+axios = require('axios'),
+username = process.argv[2],
+api = 'https://api.github.com/'
 chalk = require('chalk');
 figlet('Github Stalk', (err,data) => {
-    if(err) throw err
-    console.log(chalk.green(data))
+if(err) throw err
+console.log(chalk.green(data))
+axios.get(api+'users/'+username).then((response) => {
+    console.log(chalk.red('👤 : ' + username + ' AKA ' + response.data.name))
+})
 })
